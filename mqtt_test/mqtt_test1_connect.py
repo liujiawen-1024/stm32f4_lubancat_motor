88bd1d9f4f6cbbfa3b3c960095a222f959210685
liujiawen-1024 <18206595193@163.com>
2026-06-21 20:24:21 +0800

    中文输出

--- mqtt_test/mqtt_test1_connect.py
+++ mqtt_test/mqtt_test1_connect.py
@@ -1,5 +1,7 @@
+# coding: gb2312
 import paho.mqtt.client as mqtt
 
+# MQTT 相关配置
 MQTT_PRODUCT_ID = "RJpPAS792o"
 MQTT_PRODUCT_NAME = "Motor_Ctrl"
 MQTT_PRODUCT_TOKEN = "version=2018-10-31&res=products%2FRJpPAS792o%2Fdevices%2FMotor_Ctrl&et=1939802874&method=md5&sign=g5TP2NJAqgrt%2BPIhMJqJgw%3D%3D"
@@ -15,22 +17,22 @@ client.username_pw_set(USERNAME,MQTT_PRODUCT_TOKEN)
 
 def on_connect(client,userdata,flags,rc):
     if rc == 0:
-        print("connect onenet ok!")
+        print(f"连接OneNET成功!")
     else:
-        print(f"connect onenet error,error id:{rc}")
+        print(f"连接OneNET失败,失败码:{rc}")
 
 def on_disconnect(client,userdata,rc):
-    print(f"connect stop:{rc}")
+    print(f"连接中断:{rc}")
 
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
 
-print(f"connecting......")
+print(f"连接中……")
 
 try:
     client.connect(MQTT_BROKER,MQTT_PORT,keepalive=120)
     client.loop_forever()
 except KeyboardInterrupt:
-    print("\nexit")
+    print("\n退出")
 except Exception as e:
-    print(f"error:{e}")
+    print(f"错误:{e}")
